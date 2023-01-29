e695864b7bfdcd7f45ea52bcf7bbe8d13b22b0a7
sploganathan <sakthiloganathan1127@gmail.com>
2023-01-29 11:01:37 -0800

    delete method added for deleting the existing files

--- routers/notes.js
+++ routers/notes.js
@@ -16,14 +16,14 @@ router.get("/", (req, res) => {
   res.send(notes);
 });
 
-//post method for creating new notes
+// post method for creating new notes
 router.post("/", (req, res) => {
   if (req.body) {
     const { title, text } = req.body;
     const newNote = {
       title,
       text,
-      uuid: uuid(),
+      id: uuid(),
     };
     notes.push(newNote);
     fs.writeFile(
@@ -35,6 +35,25 @@ router.post("/", (req, res) => {
     res.send(newNote);
   } else {
     res.json("Error in posting");
+  }
+});
+
+// delete method for deleting the existing notes
+router.delete("/:id", (req, res) => {
+  if (req.params.id) {
+    const { id } = req.params;
+    // Splice - When using splice we should specify which index of the element to be deleted and also how many elements. Difference between Splice and Slice is the Splice will update the existing array whereas Slice will create a new array of elements and update that,leaving the original one.
+    const index = notes.findIndex((note) => note.id === id);
+    notes.splice(index, 1);
+    fs.writeFile(
+      path.join(__dirname, "../db/db.json"),
+      JSON.stringify(notes, null, 4),
+      (err) =>
+        err ? console.error(err) : console.log("File written successfully")
+    );
+    res.json("Successfully Deleted");
+  } else {
+    res.json("Error");
   }
 });
 
